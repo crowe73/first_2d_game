158993659ebb759afc1ac1062234305bee7e92ac
crowe73 <rowechad73@yahoo.com>
2026-05-09 23:28:41 -0500

    Player Design.
    Add player ship to scene
    Add collision shape to player
    Animate ship and exhaust
    Add script to player for movement

---
+++ Scripts/player.gd
@@ -0,0 +1,24 @@
+# Script player.gd.
+extends Area2D
+
+@onready var screensize = get_viewport_rect().size
+
+@export var speed = 150
+
+func _process(delta):
+	move_player(delta)
+	
+func move_player(delta):
+	var input = Input.get_vector("left", "right", "up", "down")
+	if input.x > 0:
+		$Ship.frame = 2
+		$Ship/Boosters.animation = "right"
+	elif input.x < 0:
+		$Ship.frame = 0
+		$Ship/Boosters.animation = "left"
+	else:
+		$Ship.frame = 1
+		$Ship/Boosters.animation = "forward"
+	position += input * speed * delta
+	position = position.clamp(Vector2(8, 8), screensize - Vector2(8, 16))
+	
